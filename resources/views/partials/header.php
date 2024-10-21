d6075b11768ee138345531ddca34bf48a7394923
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2024-10-21 16:21:21 -0300

    added partial

---
+++ resources/views/partials/header.php
@@ -0,0 +1,15 @@
+<header
+	class="flex justify-between items-center h-20 p-2 bg-[linear-gradient(to_bottom,var(--bg-primary),var(--bg-tertiary))]">
+	<img
+		class="h-full"
+		src="/img/logo.png"
+		alt="Logo del centro de formación profesional de la Criolla">
+	<nav class="flex justify-between items-center px-4">
+		<ul class="flex justify-between items-center gap-10">
+			<li class="border-transparent border-b-2 hover:border-white h-full"><a href="/">Inicio</a></li>
+			<li class="border-transparent border-b-2 hover:border-white h-full"><a href="/quienes-somos">Quienes somos</a></li>
+			<li class="border-transparent border-b-2 hover:border-white h-full"><a href="/trayectos">Trayectos</a></li>
+			<li class="border-transparent border-b-2 hover:border-white h-full"><a href="/contactos">Contacto</a></li>
+		</ul>
+	</nav>
+</header>
