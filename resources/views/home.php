245b0f292417890d5578716c4c6b518857caaae2
Ferchupessoadev <fernandomatiaspessoa471@gmail.com>
2024-10-21 16:22:03 -0300

    added view home

--- resources/views/home.php
+++ resources/views/home.php
@@ -5,12 +5,16 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Incio</title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="/css/app.css">
 	<script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-	<h1>Hola</h1>
-	<?php print_r($_SESSION['username']); ?>
+	<?php include 'partials/header.php'; ?>
+	<?php if (isset($_SESSION['username'])): ?>
+		<?= $_SESSION['username'] ?>
+	<?php endif ?>
 </body>
 
 </html>
