412653ef2d1e98c7315b57202e6fba549d1b732b
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-11-05 21:33:10 -0300

    added layouts in views with Twig

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Controllers\HomeController;
 use App\Controllers\LoginController;
 use Spyframe\lib\Route;
 
-// static routes.
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/quienes-somos', [HomeController::class, 'info']);
 Route::get('/trayectos', [HomeController::class, 'trayectos']);
@@ -13,8 +12,10 @@ Route::get('/contacto', [HomeController::class, 'contact']);
 
 // Routes for login
 Route::get('/login', [LoginController::class, 'index']);
-Route::get('/admin', [DashboardController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/logout', [LoginController::class, 'logout']);
+
+// Routes for admins
+Route::get('/admin', [DashboardController::class, 'index']);
 
 Route::start();
