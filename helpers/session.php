492a00f79f9d521de0512ec839af3485e7393c7f
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-11-07 16:36:41 -0300

    added session's integrations

--- helpers/session.php
+++ helpers/session.php
@@ -1,10 +1,38 @@
 <?php
 
+if (!function_exists('startSession')) {
+	function startSession()
+	{
+		session_start();
+	}
+}
+
 if (!function_exists('session')) {
 	function endSession()
 	{
 		session_destroy();
 		session_unset();
 		setcookie('APPSESSID', '', time() - 3600, '/');
+	}
+}
+
+if (!function_exists("getSessionId")) {
+	function getSessionId()
+	{
+		return session_id();
+	}
+}
+
+if (!function_exists("setSession")) {
+	function setSession($key, $value)
+	{
+		$_SESSION[$key] = $value;
+	}
+}
+
+if (!function_exists("getSession")) {
+	function getSession($key)
+	{
+		return $_SESSION[$key] ?? "La variable de sesión '$key' no existe";
 	}
 }
