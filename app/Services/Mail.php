4638558797a0a353c7589d6778665f656b76d725
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-11-20 15:15:02 -0300

    Formated code

--- app/Services/Mail.php
+++ app/Services/Mail.php
@@ -1,60 +1,58 @@
 <?php
 
-
 namespace App\Services;
 
-use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class Mail
 {
-	/**
-	 * Server settings
-	 * @param PHPMailer $mail
-	 *
-	 */
-	private static function configureMailer(PHPMailer $mail): void
-	{
-		$mail->isSMTP();											  // Enable SMTP
-		$mail->Host = 'smtp.hostinger.com';                           // Set the SMTP server to send through
-		$mail->SMTPAuth = true;                                       // Enable SMTP authentication
-		$mail->Username = $_ENV['MAIL_USER'];                         // SMTP username
-		$mail->Password = $_ENV['MAIL_PASS'];                         // SMTP password
-		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;              // Enable implicit TLS encryption
-		$mail->Port = $_ENV['MAIL_PORT'];                             // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-		$mail->CharSet = 'UTF-8';                                     // Set the charset
-		$mail->setFrom($_ENV['MAIL_USER'], $_ENV['MAIL_NAME']);        // Set who the message is to be sent from
-	}
+    /**
+     * Server settings
+     * @param PHPMailer $mail
+     */
+    private static function configureMailer(PHPMailer $mail): void
+    {
+        $mail->isSMTP();  // Enable SMTP
+        $mail->Host = $_ENV['MAIL_HOST'];  // Set the SMTP server to send through
+        $mail->SMTPAuth = true;  // Enable SMTP authentication
+        $mail->Username = $_ENV['MAIL_USER'];  // SMTP username
+        $mail->Password = $_ENV['MAIL_PASS'];  // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  // Enable implicit TLS encryption
+        $mail->Port = $_ENV['MAIL_PORT'];  // TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->CharSet = 'UTF-8';  // Set the charset
+        $mail->setFrom($_ENV['MAIL_USER'], $_ENV['MAIL_NAME']);  // Set who the message is to be sent from
+    }
 
-	/**
-	 * Send email
-	 * @param string $to
-	 * @param string $subject
-	 * @param string $body
-	 * @return string 
-	 * Returns a message of success or failure
-	 */
-	public static function send(string $to, string $subject, string $body): string
-	{
-		$mail = new PHPMailer(true);
+    /**
+     * Send email
+     * @param string $to
+     * @param string $subject
+     * @param string $body
+     * @return string
+     * Returns a message of success or failure
+     */
+    public static function send(string $to, string $subject, string $body): string
+    {
+        $mail = new PHPMailer(true);
 
-		try {
-			// Server settings
-			self::configureMailer($mail);
+        try {
+            // Server settings
+            self::configureMailer($mail);
 
-			// Recipients
-			$mail->addAddress($_ENV['MAIL_RECEIVER'], $_ENV['MAIL_NAME_RECEIVER']);     // Add a recipient
+            // Recipients
+            $mail->addAddress($_ENV['MAIL_RECEIVER'], $_ENV['MAIL_NAME_RECEIVER']);  // Add a recipient
 
-			// Content
-			$mail->isHTML(true);
-			$mail->Subject = $subject;
-			$mail->Body = $body;
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $body;
 
-			// Send email
-			$mail->send();
-			return "Correo enviado con éxito";
-		} catch (Exception) {
-			return "Error al enviar el correo, vuelva a intentarlo en unos minutos";
-		}
-	}
+            // Send email
+            $mail->send();
+            return 'Correo enviado con éxito';
+        } catch (Exception) {
+            return 'Error al enviar el correo, vuelva a intentarlo en unos minutos';
+        }
+    }
 }
