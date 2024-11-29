cc2c96f1643d8f4e1090adac315aeaa707115434
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-11-29 13:06:43 -0300

    Add endpoint for get all multimedia

--- app/Controllers/MultimediaController.php
+++ app/Controllers/MultimediaController.php
@@ -7,154 +7,178 @@ use App\Middlewares\CsrfMiddleware;
 
 class MultimediaController extends Controller
 {
-	/**
-	 * Función para eliminar multimedia
-	 * @return array
-	 */
-	public function deleteMultimedia(): array
-	{
-		// middlewares
-		$authMiddleware = new AuthMiddleware();
-		$csrfMiddleware = new CsrfMiddleware();
-		$authMiddleware->setNext($csrfMiddleware);
-		$response = $authMiddleware->handle();
+    /**
+     * Función para eliminar multimedia
+     * @return array
+     */
+    public function deleteMultimedia(): array
+    {
+        // middlewares
+        $authMiddleware = new AuthMiddleware();
+        $csrfMiddleware = new CsrfMiddleware();
+        $authMiddleware->setNext($csrfMiddleware);
+        $response = $authMiddleware->handle();
 
-		if (!$response['success'] && $response['message'] == 'You are not logged in') {
-			http_response_code(401);
-			return [
-				'success' => false,
-				'message' => 'No estás autorizado'
-			];
-		}
+        if (!$response['success'] && $response['message'] == 'You are not logged in') {
+            http_response_code(401);
+            return [
+                'success' => false,
+                'message' => 'No estás autorizado'
+            ];
+        }
 
-		if (!$response['success'] && $response['message'] == 'CSRF token inválido') {
-			http_response_code(400);
-			return [
-				'success' => false,
-				'message' => $response['message']
-			];
-		}
+        if (!$response['success'] && $response['message'] == 'CSRF token inválido') {
+            http_response_code(400);
+            return [
+                'success' => false,
+                'message' => $response['message']
+            ];
+        }
 
-		$directoryPath = __DIR__ . '/../../public_html';
+        $directoryPath = __DIR__ . '/../../public_html';
 
-		$file = $_POST['file'];
+        $file = $_POST['file'];
 
-		if (!file_exists($directoryPath . $file)) {
-			http_response_code(400);
+        if (!file_exists($directoryPath . $file)) {
+            http_response_code(400);
 
-			return [
-				'success' => false,
-				'message' => 'El archivo no existe',
-				'path' => $directoryPath . $file
-			];
-		}
+            return [
+                'success' => false,
+                'message' => 'El archivo no existe',
+                'path' => $directoryPath . $file
+            ];
+        }
 
-		if (!unlink($directoryPath . $file)) {
-			http_response_code(400);
+        if (!unlink($directoryPath . $file)) {
+            http_response_code(400);
 
-			return [
-				'success' => false,
-				'message' => 'Error al eliminar el archivo',
-				'path' => $directoryPath . $file
-			];
-		}
+            return [
+                'success' => false,
+                'message' => 'Error al eliminar el archivo',
+                'path' => $directoryPath . $file
+            ];
+        }
 
-		return [
-			'success' => true,
-			'message' => 'El archivo se ha eliminado correctamente',
-			'path' => $directoryPath . $file
-		];
-	}
+        return [
+            'success' => true,
+            'message' => 'El archivo se ha eliminado correctamente',
+            'path' => $directoryPath . $file
+        ];
+    }
 
-	/**
-	 * Función para subir multimedia
-	 * @return array
-	 */
-	public function uploadMultimedia(): array
-	{
-		// middlewares
-		$authMiddleware = new AuthMiddleware();
-		$csrfMiddleware = new CsrfMiddleware();
-		$authMiddleware->setNext($csrfMiddleware);
-		$response = $authMiddleware->handle();
+    /**
+     * Función para subir multimedia
+     * @return array
+     */
+    public function uploadMultimedia(): array
+    {
+        // middlewares
+        $authMiddleware = new AuthMiddleware();
+        $csrfMiddleware = new CsrfMiddleware();
+        $authMiddleware->setNext($csrfMiddleware);
+        $response = $authMiddleware->handle();
 
-		if (!$response['success'] && $response['message'] == 'You are not logged in') {
-			http_response_code(401);
-			return [
-				'success' => false,
-				'message' => 'No estás autorizado'
-			];
-		}
+        if (!$response['success'] && $response['message'] == 'You are not logged in') {
+            http_response_code(401);
+            return [
+                'success' => false,
+                'message' => 'No estás autorizado'
+            ];
+        }
 
-		if (!$response['success'] && $response['message'] == 'CSRF token inválido') {
-			http_response_code(400);
-			return [
-				'success' => false,
-				'message' => $response['message']
-			];
-		}
+        if (!$response['success'] && $response['message'] == 'CSRF token inválido') {
+            http_response_code(400);
+            return [
+                'success' => false,
+                'message' => $response['message']
+            ];
+        }
 
-		if (isset($_FILES['file']) && is_array($_FILES['file']['tmp_name'])) {
-			$uploadedFiles = $_FILES['file'];
-		} else {
-			$uploadedFiles = [
-				'tmp_name' => [$_FILES['file']['tmp_name']],
-				'name' => [$_FILES['file']['name']],
-				'size' => [$_FILES['file']['size']],
-				'type' => [$_FILES['file']['type']]
-			];
-		}
+        if (isset($_FILES['file']) && is_array($_FILES['file']['tmp_name'])) {
+            $uploadedFiles = $_FILES['file'];
+        } else {
+            $uploadedFiles = [
+                'tmp_name' => [$_FILES['file']['tmp_name']],
+                'name' => [$_FILES['file']['name']],
+                'size' => [$_FILES['file']['size']],
+                'type' => [$_FILES['file']['type']]
+            ];
+        }
 
-		$allowMimeTypes = ['image/jpeg', 'image/png', 'image/webp', 'image/gif', 'image/bmp'];
+        $allowMimeTypes = ['image/jpeg', 'image/png', 'image/webp', 'image/gif', 'image/bmp'];
 
-		$uploadResults = [];
+        $uploadResults = [];
 
-		foreach ($uploadedFiles['tmp_name'] as $key => $tmpName) {
-			$fileName = $uploadedFiles['name'][$key];
-			$fileSize = $uploadedFiles['size'][$key];
-			$fileType = $uploadedFiles['type'][$key];
-			$uploadResult = [
-				'file' => $fileName,
-				'success' => false,
-				'message' => '',
-				'path' => '',
-				'mimeTypes' => $fileType
-			];
+        foreach ($uploadedFiles['tmp_name'] as $key => $tmpName) {
+            $fileName = $uploadedFiles['name'][$key];
+            $fileSize = $uploadedFiles['size'][$key];
+            $fileType = $uploadedFiles['type'][$key];
+            $uploadResult = [
+                'file' => $fileName,
+                'success' => false,
+                'message' => '',
+                'path' => '',
+                'mimeTypes' => $fileType
+            ];
 
-			if (!in_array($fileType, $allowMimeTypes)) {
-				$uploadResult['message'] = 'Formato de archivo no permitido';
-				$uploadResults[] = $uploadResult;
-				continue;
-			}
+            if (!in_array($fileType, $allowMimeTypes)) {
+                $uploadResult['message'] = 'Formato de archivo no permitido';
+                $uploadResults[] = $uploadResult;
+                continue;
+            }
 
-			// max file size
-			if ($fileSize > 5000000) {
-				$uploadResult['message'] = 'Tamaño de archivo demasiado grande';
-				$uploadResults[] = $uploadResult;
-				continue;
-			}
+            // max file size
+            if ($fileSize > 5000000) {
+                $uploadResult['message'] = 'Tamaño de archivo demasiado grande';
+                $uploadResults[] = $uploadResult;
+                continue;
+            }
 
-			$destination = __DIR__ . '/../../public_html/multimedia/' . $fileName;
-			if (file_exists($destination)) {
-				$fileName = time() . '-' . $fileName;
-				$destination = __DIR__ . '/../../public_html/multimedia/' . $fileName;
-			}
+            $destination = __DIR__ . '/../../public_html/multimedia/' . $fileName;
+            if (file_exists($destination)) {
+                $fileName = time() . '-' . $fileName;
+                $destination = __DIR__ . '/../../public_html/multimedia/' . $fileName;
+            }
 
-			if (move_uploaded_file($tmpName, $destination)) {
-				$uploadResult['success'] = true;
-				$uploadResult['message'] = 'Archivo subido correctamente';
-				$uploadResult['path'] = '/multimedia/' . $fileName;
-			} else {
-				$uploadResult['message'] = 'Error al mover el archivo';
-			}
+            if (move_uploaded_file($tmpName, $destination)) {
+                $uploadResult['success'] = true;
+                $uploadResult['message'] = 'Archivo subido correctamente';
+                $uploadResult['path'] = '/multimedia/' . $fileName;
+            } else {
+                $uploadResult['message'] = 'Error al mover el archivo';
+            }
 
-			$uploadResults[] = $uploadResult;
-		}
+            $uploadResults[] = $uploadResult;
+        }
 
-		return [
-			'success' => true,
-			'message' => 'Archivos procesados.',
-			'files' => $uploadResults
-		];
-	}
+        return [
+            'success' => true,
+            'message' => 'Archivos procesados.',
+            'files' => $uploadResults
+        ];
+    }
+
+    public function getMultimedia(): string
+    {
+        $authMiddleware = new AuthMiddleware();
+        $result = $authMiddleware->handle();
+
+        if (!$result['success']) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+            return view('unauthorized', ['message' => 'que tamo haciendo, no estas autorizado para entrar Sharia', 'ip' => $ip]);
+        }
+
+        $files = scandir(__DIR__ . '/../../public_html/multimedia');
+
+        $files = array_diff($files, array('.', '..'));
+
+        $data = array_map(function ($file) {
+            return '/multimedia/' . $file;
+        }, $files);
+
+        return view('administrator.multimedia', [
+            'logged' => $result,
+            'data' => $data
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Spyframe\lib\Route;
 
 Route::post(uri: '/api/multimedia/upload', callback: [MultimediaController::class, 'uploadMultimedia']);
 Route::post(uri: '/api/multimedia/delete', callback: [MultimediaController::class, 'deleteMultimedia']);
+Route::get(uri: '/api/multimedia/all', callback: [CoursesController::class, 'getMultimedia']);
 Route::get(uri: '/api/courses/', callback: [CoursesController::class, 'getCourses']);
